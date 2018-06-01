2214cf12e4ef33f1d862ab08a0d2b667e1fbc4e9
xiangzhangshu1314 <38484463+xiangzhangshu1314@users.noreply.github.com>
2018-06-01 09:00:37 +0800

    Update hd.js

--- js/hd.js
+++ js/hd.js
@@ -1,8 +1,6 @@
 //必须的js代码
 var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        direction: 'vertical',
+       
         onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
     swiperAnimateCache(swiper); //隐藏动画元素 
     swiperAnimate(swiper); //初始化完成开始动画
